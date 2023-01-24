6b7c16a73746cc5e5af936750a9999b8ce3cfbfd
pkorytowski <pkorytowski@hotmail.com>
2023-01-24 22:30:14 +0100

    Wrap corsFilter into Configuration class

--- src/main/java/com/example/translation/TranslationApplication.java
+++ src/main/java/com/example/translation/TranslationApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -41,16 +42,20 @@ public class TranslationApplication {
 
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("*"));
-		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
+	@Configuration
+	public class CorsFilterConfig {
+		@Bean
+		public CorsFilter corsFilter() {
+			final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			final CorsConfiguration config = new CorsConfiguration();
+			config.setAllowCredentials(true);
+			config.setAllowedOrigins(Collections.singletonList("*"));
+			config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+			config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+			source.registerCorsConfiguration("/**", config);
+			return new CorsFilter(source);
+		}
 	}
+
 
 }
